3a840f76528818a4ae29e3bd9c75614875aaf854
unknown <chue.vang0@gmail.com>
2021-01-29 14:01:26 -0600

    Implemented identity roles.

---
+++ Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Address { get; set; }
+        public string Zipcode { get; set; }
+        public string Pickup_Day { get; set; }
+        public DateTime Extra_Pickup { get; set; }
+        public DateTime Start_Suspend_Date { get; set; }
+        public DateTime End_Suspend_Date { get; set; }
+        public int Charges_Owe { get; set; }
+        public bool Confirm_Pickup { get; set; }
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentitiyUser IdentitiyUser { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Zipcode { get; set; }
+    }
+}
